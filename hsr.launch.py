4f6e4967822b2ff9516a6365ceb1c7a697885241
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2024-11-13 16:08:23 +0900

    add launchfile for ros2 navigation stack

---
+++ hsr.launch.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription, LaunchContext
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node, SetParameter
+
+
+def declare_arguments():
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'description_package',
+            default_value='hsrb_description',
+            description='Description package with robot URDF/xacro files.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'description_file',
+            default_value='hsrb4s.urdf.xacro',
+            description='URDF/XACRO description file with the robot.',
+        )
+    )
+    return declared_arguments
+
+
+def render_xacro_and_launch_robot_state_publisher(context: LaunchContext, args: dict) -> str:
+    robot_description_content = xacro.process_file(
+        os.path.join(
+            get_package_share_directory(
+                context.perform_substitution(args['description_package'])
+            ),
+            'robots',
+            context.perform_substitution(args['description_file']),
+        ),
+        mappings={
+            'gazebo_sim': 'False',
+            'rviz_sim': 'False',
+        },
+    ).toxml()
+    return [
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[
+                {'robot_description': robot_description_content},
+            ],
+            remappings=[('joint_states', '/whole_body/joint_states')],
+            output={'both': 'log'},
+        )
+    ]
+
+
+def generate_launch_description():
+    args = {}
+    for arg in declare_arguments():
+        args[arg.name] = LaunchConfiguration(arg.name)
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        parameters=[
+            {'source_list': ['/hsrb/joint_states']},
+        ],
+        namespace='whole_body',
+        remappings=[('robot_description', '/robot_description')],
+    )
+
+    robot_state_publisher = OpaqueFunction(function=render_xacro_and_launch_robot_state_publisher, args=[args])
+
+    nav = GroupAction(
+        actions=[
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        os.path.join(
+                            get_package_share_directory('hsrb_rosnav_config'),
+                            'launch',
+                            'navigation_launch.py',
+                        )
+                    ]
+                ),
+                launch_arguments={
+                    'use_sim_time': 'true',
+                    'map': '/map.yaml'
+                }.items(),
+            ),
+        ]
+    )
+
+    nodes = [
+        joint_state_publisher,
+        robot_state_publisher,
+        nav
+    ]
+
+    return LaunchDescription(
+        declare_arguments() + [SetParameter(name='use_sim_time', value='true'),] + nodes
+    )
