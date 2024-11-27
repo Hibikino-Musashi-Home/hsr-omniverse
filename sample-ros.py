971edc83ebe028721c1d650d9d46905d6ed7e5a0
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2024-11-27 11:04:57 +0900

    implement model state api to make task evaluators to work

--- sample-ros.py
+++ sample-ros.py
@@ -9,6 +9,7 @@ kit.set_setting("/app/extensions/installUntrustedExtensions", True)
 
 import sys
 import os
+import math
 import numpy as np
 import xml.etree.ElementTree as ET
 import xacro
@@ -20,11 +21,20 @@ from omni.isaac.core.utils.rotations import euler_angles_to_quat
 import omni.kit.commands
 from omni.isaac.version import get_version
 import hsr
-from pxr import Sdf, Gf, UsdPhysics
+from pxr import Sdf, Usd, UsdGeom, Gf, UsdPhysics
 
 sys.path.append(os.path.dirname(__file__) + '/tmc_wrs_gazebo/tmc_wrs_gazebo_worlds/src')
 
 from tmc_wrs_gazebo_worlds import randomizer
+
+is_ros2 = False
+try:
+    import rclpy
+    from gazebo_msgs.srv import GetWorldProperties, GetWorldProperties_Response as GetWorldPropertiesResponse, GetModelState, GetModelState_Response as GetModelStateResponse
+    is_ros2 = True
+except ImportError:
+    import rospy
+    from gazebo_msgs.srv import GetWorldProperties, GetWorldPropertiesResponse, GetModelState, GetModelStateResponse
 
 try:
     import rosgraph
@@ -69,6 +79,8 @@ BACKGROUND_USD_PATH = "/Isaac/Environments/Grid/default_environment.usd"
 
 stage.add_reference_to_stage(assets_root_path + BACKGROUND_USD_PATH, BACKGROUND_STAGE_PATH)
 
+model_names = []
+
 # Extract poses of objects from the world file
 world_file = os.path.dirname(os.path.abspath(__file__)) + "/tmc_wrs_gazebo/tmc_wrs_gazebo_worlds/worlds/wrs2020.world.xacro"
 tree = ET.ElementTree(ET.fromstring(xacro.process_file(world_file, mappings={'trofast_knob': 'true'}).toxml()))
@@ -91,9 +103,11 @@ for i in root.findall('world/include'):
         root_joint.CreateLocalRot0Attr().Set(Gf.Quatf(1.0))
         root_joint.CreateLocalPos1Attr().Set(Gf.Vec3f(0.0))
         root_joint.CreateLocalRot1Attr().Set(Gf.Quatf(1.0))
+    model_names.append(model_name)
 
 
 def drop_object(gazebo_name, name, x, y, z, yaw):
+    global model_names
     print(f'Drop {name} ({x}, {y}, {z}, {yaw})')
     stage_path = f'/{gazebo_name.replace("-", "_")}'
     model_path = os.path.dirname(os.path.abspath(__file__)) + '/usd/wrc_models/' + name + '/model.usd'
@@ -101,24 +115,76 @@ def drop_object(gazebo_name, name, x, y, z, yaw):
         return
     create_prim(prim_path=stage_path, prim_type="Xform", translation=[x, y, z], orientation=euler_angles_to_quat([0, 0, yaw]))
     stage.add_reference_to_stage(model_path, Sdf.Path(stage_path))
+    model_names.append(gazebo_name)
 
 
 randomizer.generate_wrs_task(drop_func=drop_object)
 
-#_hsr = hsr.hsr()
+hsr_stage_path = "/hsrb"
+create_prim(prim_path=hsr_stage_path, prim_type="Xform", translation=[-2.1, 1.2, 0], orientation=euler_angles_to_quat([0, 0, -math.pi/2]))
+
+_hsr = hsr.hsr(stage_path=hsr_stage_path)
 
 # Start simulation
 kit.update()
 simulation_context = SimulationContext(stage_units_in_meters=1.0)
 kit.update()
-#_hsr.onsimulationstart(simulation_context)
+_hsr.onsimulationstart(simulation_context)
 simulation_context.initialize_physics()
 omni.timeline.get_timeline_interface().play()
+
+# simulate gazebo ros APIs required for task evaluators
+def handle_get_world_properties(req):
+    ret = GetWorldPropertiesResponse()
+    ret.model_names = model_names
+    ret.success = True
+    return ret
+
+def get_xform(stage, model_name):
+    try:
+        name = model_name.replace('::link', '').replace('-', '_')
+        prim = stage.GetPrimAtPath(f'/{name}/link')
+        if not prim.IsValid():
+            prim = stage.GetPrimAtPath(f'/{name}/body')
+        if not prim.IsValid():
+            prim = stage.GetPrimAtPath(f'/{name}/hsrb')
+        return UsdGeom.Xformable(prim).ComputeLocalToWorldTransform(Usd.TimeCode.Default())
+    except:
+        print(f'Failed to get xform for {model_name}')
+        return Gf.Matrix4d()
+
+def handle_get_model_state(req):
+    stage = omni.usd.get_context().get_stage()
+    objxform = get_xform(stage, req.model_name)
+    refxform = get_xform(stage, req.relative_entity_name)
+    relpose = refxform.GetInverse() * objxform
+    translation = relpose.ExtractTranslation()
+    rotation = relpose.GetOrthonormalized().ExtractRotationQuat()
+    rotation_imaginary = rotation.GetImaginary()
+    # create response
+    ret = GetModelStateResponse()
+    ret.header.frame_id = req.relative_entity_name
+    ret.pose.position.x = translation[0]
+    ret.pose.position.y = translation[1]
+    ret.pose.position.z = translation[2]
+    ret.pose.orientation.x = rotation_imaginary[0]
+    ret.pose.orientation.y = rotation_imaginary[1]
+    ret.pose.orientation.z = rotation_imaginary[2]
+    ret.pose.orientation.w = rotation.GetReal()
+    ret.success = True
+    return ret
+
+if is_ros2:
+    _hsr.ros2node.create_service('/gazebo/get_world_properties', GetWorldProperties, handle_get_world_properties)
+    _hsr.ros2node.create_service('/gazebo/get_model_state', GetModelState, handle_get_model_state)
+else:
+    rospy.Service('/gazebo/get_world_properties', GetWorldProperties, handle_get_world_properties)
+    rospy.Service('/gazebo/get_model_state', GetModelState, handle_get_model_state)
 
 while kit.is_running():
     # Run with a fixed step size
     simulation_context.step(render=True)
-    #_hsr.step()
+    _hsr.step()
 
 simulation_context.stop()
 kit.close()
